c93b3d3a0781fdf68745fe38389298a33249c796
Dan Duffy <de4db1t@gmail.com>
2019-07-15 23:09:35 +0100

    Initial connection to Slack RTM/Web Client and printing bot mention text

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import time
+import slack
 
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+rtm_client = slack.RTMClient(token=slack_token)
+web_client = slack.WebClient(token=slack_token)
+
+
+@rtm_client.run_on(event='message')
+def bot_mentioned(**payload):
+    data = payload['data']
+    text = data['text']
+    print('<@{}>'.format(rtm_client.slack_bot_id))
+    if ('<@{}>'.format(rtm_client.slack_bot_id)) in text:
+        parse_query(text)
+
+
+@rtm_client.run_on(event='hello')
+def bot_connected(**payload):
+    rtm_client.slack_bot_id = web_client.auth_test()['user_id']
+    print('Bot {} connected to RTM!'.format(rtm_client.slack_bot_id))
+
+
+def parse_query(text):
+    print(text)
 
 def main():
-    ''' Entrypoint for slack-cve bot.'''
-    print(1)
+    print("Starting Slackbot!")
+    rtm_client.start()
 
 
 if __name__ == '__main__':
